5b60dadc5a797117b1e0a173d8e86c19d5d2f460
leirisson <103783610+leirisson@users.noreply.github.com>
2025-10-17 22:58:54 -0400

    Refactor text generation logic to use async/await and improve response handling

--- 1-introducao/src/index.ts
+++ 1-introducao/src/index.ts
@@ -4,23 +4,37 @@ dotenv.config()
 import OpenAI from "openai"
 const cliente = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 
-cliente.chat.completions.create({
-    model: "gpt-4.1-mini",
-    messages: [
-        {
-            role: "system",
-            content: "limite suas respostas em 300 caracteres, você é um asisten que só responde perguntas de inglês, você não pode responder sobre outros tipos de conteudos que não seja ingles.",
 
-        },
-        {
-            role: "user",
-            content: "como conjugar o verto tobe de forma fácil ?"
-        }
-    ]
-})
-    .then((completions) => {
-        console.log(completions.choices[0].message.content)
+async function gerarTexto() {
+    const completitions = await cliente.chat.completions.create({
+        model: "gpt-4.1-mini",
+        max_completion_tokens: 20,
+        temperature: 0.5,
+        messages: [
+            {
+                role: "developer",
+                content: "limite suas respostas em 300 caracteres, você é um asisten que só responde perguntas de inglês, você não pode responder sobre outros tipos de conteudos que não seja ingles.",
+
+            },
+            {
+                role: "user",
+                content: "como conjugar o verto tobe de forma fácil ?"
+            }
+        ]
     })
+
+    const msg_response =  await completitions.choices[0].message.content
+
+    console.log(msg_response)
+}
+
+gerarTexto()
+
+
+
+
+
+
 
 
 
